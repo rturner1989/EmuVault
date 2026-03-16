401e239f61c1aa34ffb22d8edd1e408b8aca9f6f
Robert Turner <robert.turner@teamfeepay.com>
2026-03-16 20:41:43 +0000

    add turbo loading bar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,9 @@ import "@hotwired/turbo"
 import "./turbo/cable_stream_source_element"
 import "./controllers"
 
+// Show progress bar immediately on navigation (default is 500ms delay)
+Turbo.config.drive.progressBarDelay = 0
+
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/serviceworker.js").catch(function (err) {
     console.warn("Service worker registration failed:", err)
