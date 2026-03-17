e49ac5972a3ff806940f6bbf57e8e18e91d468c3
Robert Turner <robert.turner@teamfeepay.com>
2026-03-17 13:47:31 +0000

    fix display of file input on mobile

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,5 +1,6 @@
 import { Application } from "@hotwired/stimulus"
 import AutoSubmitController from "./auto_submit_controller"
+import FilePickerController from "./file_picker_controller"
 import DialogController from "./dialog_controller"
 import FlashController from "./flash_controller"
 import DirectoryBrowserController from "./directory_browser_controller"
@@ -11,6 +12,7 @@ import SaveHintController from "./save_hint_controller"
 
 const application = Application.start()
 application.register("auto-submit", AutoSubmitController)
+application.register("file-picker", FilePickerController)
 application.register("dialog", DialogController)
 application.register("directory-browser", DirectoryBrowserController)
 application.register("notifications", NotificationsController)
