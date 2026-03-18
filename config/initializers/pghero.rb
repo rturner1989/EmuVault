33f2dc81937f676cf99603bb978307c05b189dd5
Robert Turner <robert.turner@teamfeepay.com>
2026-03-18 14:06:09 +0000

    remove need for credentials when viewing pghreo and sidekiq

--- config/initializers/pghero.rb
+++ config/initializers/pghero.rb
@@ -1,7 +1,1 @@
-PgHero::Engine.middleware.use(Rack::Auth::Basic, "EmuVault") do |username, password|
-  admin_email = ENV.fetch("ADMIN_EMAIL", nil)
-  admin_password = ENV.fetch("ADMIN_PASSWORD", nil)
-  admin_email.present? &&
-    ActiveSupport::SecurityUtils.secure_compare(username, admin_email) &&
-    ActiveSupport::SecurityUtils.secure_compare(password, admin_password.to_s)
-end
+# No additional PgHero configuration needed for self-hosted use.
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -12,10 +12,3 @@ Sidekiq.configure_client do |config|
 end
 
 require "sidekiq/web"
-Sidekiq::Web.use(Rack::Auth::Basic, "EmuVault") do |username, password|
-  admin_email = ENV.fetch("ADMIN_EMAIL", nil)
-  admin_password = ENV.fetch("ADMIN_PASSWORD", nil)
-  admin_email.present? &&
-    ActiveSupport::SecurityUtils.secure_compare(username, admin_email) &&
-    ActiveSupport::SecurityUtils.secure_compare(password, admin_password.to_s)
-end
