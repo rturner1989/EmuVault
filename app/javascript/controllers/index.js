f3604b8d70a83b249e69deaf8a426939256bf9a4
Robert Turner <robert.turner@teamfeepay.com>
2026-03-21 21:06:43 +0000

    adjust index.js layout for readability

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,31 +1,45 @@
 import { Application } from "@hotwired/stimulus"
-import AutoSubmitController from "./auto_submit_controller"
-import FilePickerController from "./file_picker_controller"
-import DialogController from "./dialog_controller"
-import FlashController from "./flash_controller"
-import IframeDownloadController from "./iframe_download_controller"
-import DirectoryBrowserController from "./directory_browser_controller"
-import NotificationsController from "./notifications_controller"
-import OnboardingController from "./onboarding_controller"
-import PushSubscriptionController from "./push_subscription_controller"
-import QuickSyncController from "./quick_sync_controller"
-import SaveHintController from "./save_hint_controller"
-import ThemeController from "./theme_controller"
-import ProfileSelectController from "./profile_select_controller"
-import SwipeDismissController from "./swipe_dismiss_controller"
 
 const application = Application.start()
+
+import AutoSubmitController from "./auto_submit_controller"
 application.register("auto-submit", AutoSubmitController)
-application.register("file-picker", FilePickerController)
+
+import DialogController from "./dialog_controller"
 application.register("dialog", DialogController)
+
+import DirectoryBrowserController from "./directory_browser_controller"
 application.register("directory-browser", DirectoryBrowserController)
-application.register("notifications", NotificationsController)
-application.register("onboarding", OnboardingController)
-application.register("push-subscription", PushSubscriptionController)
-application.register("quick-sync", QuickSyncController)
+
+import FilePickerController from "./file_picker_controller"
+application.register("file-picker", FilePickerController)
+
+import FlashController from "./flash_controller"
 application.register("flash", FlashController)
+
+import IframeDownloadController from "./iframe_download_controller"
 application.register("iframe-download", IframeDownloadController)
-application.register("save-hint", SaveHintController)
-application.register("theme", ThemeController)
+
+import NotificationsController from "./notifications_controller"
+application.register("notifications", NotificationsController)
+
+import OnboardingController from "./onboarding_controller"
+application.register("onboarding", OnboardingController)
+
+import ProfileSelectController from "./profile_select_controller"
 application.register("profile-select", ProfileSelectController)
+
+import PushSubscriptionController from "./push_subscription_controller"
+application.register("push-subscription", PushSubscriptionController)
+
+import QuickSyncController from "./quick_sync_controller"
+application.register("quick-sync", QuickSyncController)
+
+import SaveHintController from "./save_hint_controller"
+application.register("save-hint", SaveHintController)
+
+import SwipeDismissController from "./swipe_dismiss_controller"
 application.register("swipe-dismiss", SwipeDismissController)
+
+import ThemeController from "./theme_controller"
+application.register("theme", ThemeController)
