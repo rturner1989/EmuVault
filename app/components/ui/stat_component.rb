a31dd0b1c2d3a11a6494ad2af1b3622f2da2af4c
Robert Turner <robturner1000@hotmail.co.uk>
2026-03-23 11:42:52 +0000

    cleaned up view templates for activity-data_imports folders

--- app/components/ui/stat_component.rb
+++ app/components/ui/stat_component.rb
@@ -3,7 +3,12 @@
 module UI
   class StatComponent < ApplicationComponent
     renders_one :icon, "UI::IconComponent"
-    renders_one :action
+    renders_one :action, ->(href:, label: "View all", icon: "fa-arrow-right", **kwargs) {
+      component = ActionComponent.new(href: href, variant: :link_primary, class: "text-xs", **kwargs)
+      component.with_trailing_icon(name: icon)
+      component.with_content(label)
+      component
+    }
 
     def initialize(title:, value:, description: nil, value_color: nil)
       @title = title
